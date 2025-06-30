a18b92f9bc68b0454282d937a505eec612812257
felipehscherer <felipe.scherepento@gmail.com>
2025-06-30 15:56:36 -0300

    fix: ajuste de metadados

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'GT-LFI',
+  description: 'Official landing page of the GT-LFI Working Group, showcasing initiatives and research on Local File Inclusion vulnerabilities and defense strategies.',
+  generator: 'GT-LFI',
 }
 
 export default function RootLayout({
